9a150c04b0c4b387cf5e10463133e15c06d77ed9
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-04-16 23:31:25 +0200

    Proper cleaning of value

--- src/app/fields/emails_field.php
+++ src/app/fields/emails_field.php
@@ -2,6 +2,59 @@
 class EmailsField extends CharField {
 
 	function __construct($options = []){
+		$options += [
+			"trim_value" => true,
+			"null_empty_output" => true,
+			"max_emails" => null,
+		];
+
+		$this->max_emails = $options["max_emails"];
+		unset($options["max_emails"]);
+
 		parent::__construct($options);
+
+		$this->update_messages([
+			"invalid" => _("Address %s is not valid email address"),
+			"duplicit" => _("The email address %s is listed more than once"),
+			"max_emails" => _("Maximum number of email addresses is %max_emails% (now it is %count%)"),
+		]);
+	}
+
+	function clean($value){
+		list($err,$value) = parent::clean($value);
+		if(!is_null($err) || !strlen($value)){ return [$err,$value]; }
+
+		$ear = new Yarri\EmailAddressRecognizer($value);
+		$items = $ear->toArray();
+
+		if(!$ear->isValid()){
+			foreach($items as $item){
+				if(!$item["valid"]){
+					$err = sprintf($this->messages["invalid"],h($item));
+					break;
+				}
+			}
+			return [$err,null];
+		}
+
+		$addresses = [];
+		foreach($items as $item){
+			$address = strtolower($item["address"]);
+			if(in_array($address,$addresses)){
+				$err = sprintf($this->messages["duplicit"],h($address));
+				return [$err,null];
+			}
+			$addresses[] = $address;
+		}
+
+		if(!is_null($this->max_emails) && sizeof($items)>$this->max_emails){
+			$err = strtr($this->messages["max_emails"],[
+				"%max_emails%" => $this->max_emails,
+				"%count%" => sizeof($items)
+			]);
+			return [$err,null];
+		}
+
+		return [null,$ear->toString()];
 	}
 }
--- test/tc_emails_field.php
+++ test/tc_emails_field.php
@@ -2,9 +2,21 @@
 class TcEmailsField extends TcBase {
 
 	function test(){
-		$this->field = new EmailsField();
+		$this->field = new EmailsField(["max_emails" => 3]);
 		
 		$value = $this->assertValid("john@doe.com");
 		$this->assertEquals("john@doe.com",$value);
+
+		$value = $this->assertValid(" john@doe.com ,  Samantha Doe <samantha@doe.com> ");
+		$this->assertEquals("john@doe.com, Samantha Doe <samantha@doe.com>",$value);
+
+		$err_msg = $this->assertInvalid("john.doe");
+		$this->assertEquals("Address john.doe is not valid email address",$err_msg);
+
+		$err_msg = $this->assertInvalid("john@doe.com, JOHN DOE <JOHN@DOE.COM>");
+		$this->assertEquals("The email address john@doe.com is listed more than once",$err_msg);
+
+		$err_msg = $this->assertInvalid("john@doe.com, samantha@doe.com, bob@doe.com, jack@doe.com");
+		$this->assertEquals("Maximum number of email addresses is 3 (now it is 4)",$err_msg);
 	}
 }
